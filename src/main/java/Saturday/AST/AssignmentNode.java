d88ff5032c2602a63cce51405773ab3403ddb527
Siphethile Sabatha Machi <simachijhb024@student.wethinkcode.co.za>
2025-10-25 15:09:57 +0200

    node class

--- src/main/java/Saturday/AST/AssignmentNode.java
+++ src/main/java/Saturday/AST/AssignmentNode.java
@@ -5,12 +5,19 @@ import Saturday.Compiler.Token;
 import java.beans.Expression;
 
 public class AssignmentNode extends Node{
-    private IdentifierNode identifierNode;
 
-    private Expression expression;
-//
-    public AssignmentNode(Token identifier, Token value) {
-        super(new IdentifierNode(identifier), new LiteralNode(value));
+
+    public AssignmentNode(Token token) {
+        super(token);
     }
 
+    /**
+     * @param env
+     * @param <Environment>
+     * @return
+     */
+    @Override
+    public <Environment> Object evaluate(Environment env) {
+        return null;
+    }
 }
